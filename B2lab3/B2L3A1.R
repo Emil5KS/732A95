c4fde6ce96b68780f6164d85c960a436931019df
Emil K Svensson <EmilsHem@Emils-MacBook-Pro.local>
2016-12-14 22:24:40 +0100

    Köttar på

--- B2lab3/B2L3A1.R
+++ B2lab3/B2L3A1.R
@@ -21,22 +21,19 @@
 # (Fuck yeah)
 
 
+library(pamr)
 
 data <- read.csv2("B2lab3/data.csv", encoding = "latin1")
 data <- data[sample(nrow(data)),]
 data$Conference <- as.factor(data$Conference)
 
-
 train  <- data[1:45, ]
 test   <- data[46:64, ]
-
-library(pamr)
 
 y <- as.factor(train$Conference)
 x <- t(train[-which(colnames(data) == "Conference")])
 
 TRAIN <- list(x = x, y = y,geneid = as.character( 1:nrow(x) ), genenames = rownames(x) ) 
-
 
 y1 <- as.factor(test$Conference)
 x1 <- t(test[-which(colnames(test) == "Conference")])
@@ -45,14 +42,14 @@ TEST <- list(x = x1, y = y1,geneid = as.character( 1:nrow(x1) ), genenames = row
 
 
 
-
-model <- pamr.train(TRAIN, threshold = seq(0,4, 0.1)) 
-
-pamr.plotcen(model, TRAIN, threshold=1)
-pamr.plotcen(model, TRAIN, threshold=2.5)
-
-firstmodel <-pamr.listgenes(model,TRAIN,threshold=2.5)
-cat( paste( colnames(data)[as.numeric(firstmodel[,1])], collapse='\n' ) )
+# 
+# model <- pamr.train(TRAIN, threshold = seq(0,4, 0.1)) 
+# 
+# pamr.plotcen(model, TRAIN, threshold=1)
+# pamr.plotcen(model, TRAIN, threshold=2.5)
+# 
+# firstmodel <-pamr.listgenes(model,TRAIN,threshold=2.5)
+# cat( paste( colnames(data)[as.numeric(firstmodel[,1])], collapse='\n' ) )
 
 cvmodel=pamr.cv(model,TRAIN)
 print(cvmodel) #13 1.2        314    4 ,  mao 314 variabler vill vi ha
@@ -100,10 +97,11 @@ library(reshape2)
 
 elasticNet<- cv.glmnet(x = as.matrix(train[,-which(colnames(train) == "Conference")]), y = train$Conference, 
        family = "binomial", alpha = 0.5)    
-  
+mycoeffs <- coefficients(elasticNet)[-1,] 
+cat(paste0(names(mycoeffs[mycoeffs != 0]),"\n")) 
+
 plot(elasticNet)    
  
-mycoeffs <- coefficients(elasticNet)[-1,] 
 colnames(mycoeffs) <- elasticNet$lambda  
 
 myElnet<-melt(as.matrix(mycoeffs),id = rownames,c("lambda","coef")) 
@@ -125,24 +123,25 @@ K   <- as.kernelMatrix(crossprod(t(as.matrix(train[,-which(colnames(train) == "C
 res <- kpca(K, kernel = "vanilladot")
 barplot(res@eig)
 plot(res@rotated[,1], res@rotated[,2], xlab="PC1", ylab="PC2")
-
+res@
 predict(res,as.matrix(test[,-4703]))
 #Funkar inte.
 ksvm() # använd den här istället
+filter <- ksvm(Conference~.,data=train,kernel="vanilladot")
+colnames(train)[filter@alphaindex[[1]]]
+
+table(predict(filter,test[,-ncol(test)]),test$Conference) 
 
 
+Conference.t.test<- as.numeric(as.character(data$Conference)) 
 
-
-
-Conference.t.test<- as.numeric(as.character(data$Conference))
-
-data.t.test <- t(apply(data,1,FUN = function(x) as.numeric(as.character(x))))
-data.t.test <- as.data.frame(data.t.test)
-colnames(data.t.test) <- colnames(data)
+data.t.test <- t(apply(data,1,FUN = function(x) as.numeric(as.character(x)))) 
+data.t.test <- as.data.frame(data.t.test) 
+colnames(data.t.test) <- colnames(data) 
 
 
 pvalues<-matrix(ncol = 2, nrow = ncol(data))
-for (i in 1:ncol(data)-1){ 
+for (i in 1:ncol(data)){ 
   pvalues[i,]<- c(colnames(data.t.test)[i] ,
                  t.test(data.t.test[,i]~Conference.t.test ,alternative = "two.sided" )$p.value) 
 }
@@ -171,16 +170,21 @@ pvalues$pvalue <- as.numeric(as.character(pvalues$pvalue))
 #   pmin(1, cummin(n/i * p[o]))[ro]
 # }
 j <- 1:ncol(data)-1
-alph <- 0.25
+alph <- 0.05
 pvalues$feature <- as.factor(pvalues$feature)
 pvalues <- pvalues[order(pvalues$pvalue),]
 pvalues$reject <- 1:nrow(pvalues)
-for (i in 1:nrow(pvalues)){
-  pvalues$reject[i]<-(ifelse(alph*(i/nrow(pvalues)) > pvalues$pvalue[i] , 0,1))
-  
-}
+
+# for (i in 1:nrow(pvalues)){
+#   pvalues$reject[i]<-(ifelse(alph*(i/nrow(pvalues)) > pvalues$pvalue[i] , 0,1))
+#   
+# }
+pvalues$reject<-(ifelse(alph*(1:nrow(pvalues)/nrow(pvalues)) > pvalues$pvalue, 0,1))
+
+nrow(pvalues[pvalues$reject == 0,]) - 1
 
 
 ggplot(data = pvalues[1:4702,], aes(x = 1:4702,y=pvalue, col = reject)) + geom_point()
+
 
 cummin(c(3:1, 2:0, 4:2))
--- B2lab3/B3L3A2.R
+++ B2lab3/B3L3A2.R
@@ -3,45 +3,133 @@ spam <- read.csv2("B2lab3/spambase.csv")
 ind <- sample(1:nrow(spam))
 spam <- spam[ind,c(1:48,58)]
 h <- 1
-beta <- # Your value here
-  M <- # Your value here
-  N <- 500 # number of training points
+betai <- -0.5  # Your value here
+  Mi <- 20 # Your value here
+  Ni <- 500 # number of training points
 
-spam$Spam[spam$Spam == 0] <- (-1) 
-
-gaussian_k <- function(x, h) { # Gaussian kernel # Your code here
-  return(exp(-(x^2/h))) 
-} 
-
-
-SVM <- function(sv,i){  
-  # SVM on point i with support vectors sv
-  # Your code here
   if(all(levels(factor(spam$Spam)) == c(-1,1)) | all(levels(factor(spam$Spam)) == c(1,-1)) ){
     
   }else{
     spam$Spam[spam$Spam == 0] <- (-1) 
   }
-  # Note that the labels in spambase.csv are 0/1 and SVMs need -1/+1. Then, use 2*label-1
-  # to convert from 0/1 to -1/+1
-  # Do not include the labels when computing the Euclidean distance between the point i
-  # and each of the support vectors. This is the distance to use in the kernel function
-  # You can use dist() to compute the Euclidean distance }
-  mydist <-as.matrix(dist(spam[,-ncol(spam)]))
-  gauss  <- apply(mydist,2,FUN = gaussian_k,h=1)
+  
+gaussian_k <- function(x, h = 1) { # Gaussian kernel # Your code here
+  return(exp(-(x/h)^2)) 
+} 
+
+
+#a_n = C- m_n
+
+
+
+SVM <- function(sv,i,M = Mi, beta = betai){  
+
+
+  #  # for speedups
+  # euclidean_d  <- function(x, xi) {
+  #   x <- t(as.matrix(x))
+  #   xi <- as.numeric(xi)
+  #   sqrt(colSums((x - xi)^2))
+  #   ## d <- dist(rbind(x, xi))
+  #   ## d[(length(d) - nrow(x) + 1):length(d)]
+  # }
+  
+  
+  step4 <- function(dataindex){
+     yxi <- c()
+     for (m in 1:M){ 
+      distelement <- dist(cbind(dataindex,spam[m,-ncol(spam)]))
+       
+     yxi[m]<-  spam[m,"Spam"]* gaussian_k(distelement)
+     #sum(1*spam[sv,"Spam"]* gaussian_k(dist(spam[i,],spam[sv,]))) + b
+    
+     }
+     return(sum(yxi))
+  }
+  
+  # step4speed <- function(dataindex){
+  #   yxi <- c()
+  #   for (m in 1:M){ 
+  #     distelement <-  euclidean_d(x=dataindex,xi=spam[m,-ncol(spam)])
+  #     
+  #     yxi[m]<-  spam[m,"Spam"]* gaussian_k(distelement)
+  #     #sum(1*spam[sv,"Spam"]* gaussian_k(dist(spam[i,],spam[sv,]))) + b
+  #     
+  #   }
+  #   return(sum(yxi))
+  # }
+  # 
+  
+  
+  step8 <- function(SV){
+    yxm <- c()
+    res<-c()
+    for (m in SV) {
+      distelement <- dist(cbind(spam[m,-ncol(spam)],spam[m,-ncol(spam)])) 
+      yxm[m] <-  spam[m,"Spam"]* gaussian_k(distelement)
+      res[m] <-  spam[m,"Spam"]*(step4(dataindex = spam[m,-ncol(spam)]) - yxm[m])
+    } 
+    return(which.max(res))
+  }  
+  
+  # 
+  # step8lapply <- function(SV){
+  #   #yxm <- c()
+  #   #res<-c()
+  #   rez<- lapply(SV,function(m){
+  #     distelement <- dist(cbind(spam[m,-ncol(spam)],spam[m,-ncol(spam)])) 
+  #     yxm <-  spam[m,"Spam"]* gaussian_k(distelement)
+  #     res <-  spam[m,"Spam"]*(step4speed(dataindex = spam[m,-ncol(spam)]) - yxm[m])
+  #     res
+  #   }) 
+  #   return(which.max(rez))
+  # }  
+  # 
+  
+  
+  b <- 0
   errors <- 1
   errorrate <- vector(length = N)
   errorrate[1] <- 1
   sv <- c(1)
+  
+  
+  
+  
   for(i in 2:N) {
-    # Your code here
-  }
-  plot(errorrate[seq(from=1, to=N, by=10)], ylim=c(0.2,0.4), type="o") length(sv)
+
+    s4<-step4(dataindex = spam[i,-ncol(spam)])
+    
+    if (spam[i,"Spam"]*s4 <= beta){
+    
+        sv[i] <- i 
+        errors <- errors + 1
+        
+      if (length(sv) > M  ){
+        
+        sv <- sv[-step8(SV = sv)]
+        
+      }
+    }
+    
+  errorrate[i] <- errors / i 
+  
+    }
+  #plot(errorrate[seq(from=1, to=N, by=10)], ylim=c(0.2,0.4), type="o") 
+  plot(errorrate)
+  length(sv)
   errorrate[N]
+  return(errorrate)
 }   
 
 
 
-#Vet du vad en gotländsk statistiker säger när han har ont i magen? 
+system.time()
+svm1<-SVM(M = 500, beta = 0)
+svm2<-SVM(M = 500, beta = -0.05)
+svm3<-SVM(M = 20, beta = 0)
+svm4<-SVM(M = 20, beta = -0.05)
+
+#Vet du vad en gotländsk statistiker säger när han är dålig i magen? 
 #Jag har gauss i magen.
   
